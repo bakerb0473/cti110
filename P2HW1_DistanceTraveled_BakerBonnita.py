6dffd587c6c6477a0e599f3146da4f3f9c08b564
bakerb0473 <40313062+bakerb0473@users.noreply.github.com>
2018-06-16 03:09:36 -0400

    First submitted

---
+++ P2HW1_DistanceTraveled_BakerBonnita.py
@@ -0,0 +1,15 @@
+#CTI-110
+#P2HW1-DistanceTraveled
+#Bonnita Baker
+#6/15/2018
+#
+
+#This program calculates rate and distance
+print("input a speed and hours of trip")
+speed = float(input("Speed(mph):"))
+time = float(input("Hours of Trip:"))
+print("Distance:", (speed * time))
+
+                       
+
+
