7f1d3ea7107a64d17040e6d1910a72c285fb889d
bakerb0473 <40313062+bakerb0473@users.noreply.github.com>
2018-07-19 23:48:17 -0400

    Add files via upload

---
+++ P4T1b_Baker.py
@@ -0,0 +1,25 @@
+# CTI-110
+# P4T1b-LastName
+# Bonnita Baker
+# 7/18/2018
+
+
+def main():
+    import turtle
+    t = turtle.Pen()
+    t.right(90)
+    t.forward(230)
+    t.left(90)
+    for x in range(180):
+        t.forward(1)
+        t.left(1)
+   
+    
+        
+        
+        
+    
+    
+    
+# Program start
+main()
---
+++ m5t1_Baker.py
@@ -0,0 +1,51 @@
+# CTI-110
+# Turtle-Triangle
+# Bonnita Baker
+# 6/28/2018
+
+
+def main():
+    import turtle
+    turtle.forward(0)
+    turtle.shape("turtle")
+    turtle.color("teal")
+    turtle.forward(100)
+    turtle.left(120)
+    turtle.forward(100)
+    turtle.left(120)
+    turtle.forward(100)
+    turtle.left(90)  
+    
+    import turtle
+    turtle.forward(0)
+    turtle.color("red")
+    turtle.forward(50)
+    turtle.left(90)
+    turtle.color("orange")
+    turtle.forward(50)
+    turtle.left(90)
+    turtle.color("yellow")
+    turtle.forward(50)
+    turtle.left(90)
+    turtle.color("green")
+    turtle.forward(50)
+    turtle.left(90) 
+    
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+#Program Start
+main()
+
